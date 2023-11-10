8801e8455ad47abb767744a2fa7f0e0a81545604
kris007iron <kris007.iron@gmail.com>
2023-11-10 10:37:08 +0100

    Update graph to display horizontal bars with color gradient and text labels

--- phpgrafika.php
+++ phpgrafika.php
@@ -3,16 +3,20 @@ for ($i = 0; $i < 10; $i++) {
     $liczby[$i] = rand() % 10;
 }
 header("Content-type: image/jpeg");
-$rysunek = imagecreate(100, 100)
+$rysunek = imagecreate(1000, 1000)
     or die("Biblioteka graficzna nie została zainstalowana!");
 $kolorbialy = imagecolorallocate($rysunek, 255, 255, 255);
 $kolorczarny = imagecolorallocate($rysunek, 0, 0, 0);
 imagefill($rysunek, 0, 0, $kolorbialy);
 
 for ($i = 0; $i < 10; $i++) {
-    $kolorslupka = imagecolorallocate($rysunek, 25 * $i, 25 * $i, 0);
-    imagefilledrectangle($rysunek, $i * 10 + 3, 90 - $liczby[$i] * 10, $i * 10 + 7, 90, $kolorslupka);
-    imagestring($rysunek, 1, 3 + $i * 10, 91, $i, $kolorczarny);
+    //make a horizontal graph wih colours from green to red graddualy
+    $kolor = imagecolorallocate($rysunek, 255 - $i * 25, $i * 25, 0);
+    //add text of i at the beggining and at the end
+    $value = $liczby[$i];
+    imagestring($rysunek, 8, 0, $i * 100 + 40, $i, $kolorczarny);
+    imagefilledrectangle($rysunek, 20, $i * 100, 1000 - $value * 100, $i * 100 + 100, $kolor);
+    imagestring($rysunek, 8, 1010 - $value * 100, ($i * 100) + 40, $value, $kolorczarny);
 }
 imagejpeg($rysunek);
 ?>
